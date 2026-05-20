7822c31fcc948983265e3d7df719fa2614bf95c4
pritam-b-dev <pritam.bhowmick@ymail.com>
2026-05-20 17:50:06 +0600

    created a post api to insert data to my db car collection

--- index.js
+++ index.js
@@ -18,16 +18,32 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+
+    //declareing db and collections starts.
+
+    const db = client.db("rentify");
+    const carCollection = db.collection("car");
+
+    //declareing db and collections end.
+    //-------------------------------------------------------
+
+    // api for cars starts
+
+    app.post("/car", async (req, res) => {
+      const carData = req.body;
+      const result = await carCollection.insertOne(carData);
+      res.json(result);
+    });
+
+    // api for cars ends
+    //------------------------------------------------------
+  } catch (error) {
+    console.error(error);
   }
 }
 run().catch(console.dir);
